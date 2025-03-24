2b3ff1609c746b3a4c3cf650d17b8d54aa08890f
AmandKorcz <amanda.korczagin@gmail.com.br>
2025-03-24 17:28:14 -0300

    database

---
+++ medsync/src/teste_banco.js
@@ -0,0 +1,11 @@
+const connection = require('./database')
+
+connection.query('SELECT * FROM medico', (err, results) => {
+    if (err) {
+        console.error('Erro ao realizar a consulta, ', err.stack)
+        return
+    }
+    console.log('Resultados da consulta:', results)
+})
+
+connection.end()
