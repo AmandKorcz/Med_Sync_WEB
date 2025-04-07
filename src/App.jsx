49814627665a69e1cfe8674d815a814302fd7031
Flavia A <fl.souza@catolicasc.edu>
2025-04-07 20:57:00 -0300

    video

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./App.jsx";
+import Principal from "./pages/Principal.jsx";
+
+function App() {
+  return (
+    <div>
+      <Principal /> 
+    </div>
+  );
+}
+
+export default App;
--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -1,23 +1,26 @@
 import Header from "../components/header.jsx";
 import Footer from "../components/footer.jsx";
-import MedSync from "../assets/image/MedSync.png"; 
-import Crianças from "../assets/image/Crianças.jpg"
+import MedSync from "../assets/image/MedSync.png";
+// import Crianças from "../assets/image/Crianças.jpg"
 import { FaFileAlt, FaCalendarAlt, FaUsers } from "react-icons/fa";
+import VidMed from "../assets/image/MedVideo.mp4";
 
 function Principal() {
   return (
     <div className="flex flex-col min-h-screen">
-    <Header />
-    <div className="w-full">
-        <img
-          src={Crianças}
-          alt="Imagem principal"
-          className="w-full h-screen object-cover object-top"
+      <Header />
+      <div className="w-full">
+        <video
+          src={VidMed}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-[80vh] object-cover object-top"
         />
       </div>
       <section className="bg-white py-12 px-4 md:px-20">
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
-
           {/* Card 1 */}
           <div className="bg-white rounded-2xl shadow-lg p-6 text-center">
             <div className="flex justify-center mb-4">
@@ -61,8 +64,8 @@ function Principal() {
               Profissionais capacitados
             </h3>
             <p className="text-gray-600 text-sm">
-              Automatize e acompanhe emails para indivíduos ou grupos. Organize sua
-              equipe com sistemas integrados.
+              Automatize e acompanhe emails para indivíduos ou grupos. Organize
+              sua equipe com sistemas integrados.
             </p>
           </div>
         </div>
@@ -72,28 +75,35 @@ function Principal() {
       <section className="bg-[#DFF3F4] py-12 px-6 md:px-20">
         <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-10">
           <div className="flex-shrink-0">
-            <img
-              src={MedSync}
-              alt="Logo MedSync"
-              className="w-60 md:w-72"
-            />
+            <img src={MedSync} alt="Logo MedSync" className="w-60 md:w-72" />
           </div>
           <div>
             <h2 className="text-2xl md:text-3xl font-bold text-[#008E9A] mb-4">
               O que somos?
             </h2>
             <p className="text-gray-800 text-base mb-4">
-              <strong>MedSync</strong> é uma plataforma moderna de agendamento de
-              consultas médicas, desenvolvida para facilitar a conexão entre
+              <strong>MedSync</strong> é uma plataforma moderna de agendamento
+              de consultas médicas, desenvolvida para facilitar a conexão entre
               pacientes e profissionais da saúde.
               <br />
-              Com uma interface intuitiva e tecnologia avançada, o MedSync permite:
+              Com uma interface intuitiva e tecnologia avançada, o MedSync
+              permite:
             </p>
             <ul className="list-disc list-inside text-gray-800 space-y-2">
-              <li><strong>Agendar consultas rapidamente</strong></li>
-              <li><strong>Gerenciar horários de forma eficiente</strong></li>
-              <li><strong>Acesso seguro com autenticação</strong></li>
-              <li><strong>Experiência simplificada para médicos e pacientes</strong></li>
+              <li>
+                <strong>Agendar consultas rapidamente</strong>
+              </li>
+              <li>
+                <strong>Gerenciar horários de forma eficiente</strong>
+              </li>
+              <li>
+                <strong>Acesso seguro com autenticação</strong>
+              </li>
+              <li>
+                <strong>
+                  Experiência simplificada para médicos e pacientes
+                </strong>
+              </li>
             </ul>
           </div>
         </div>
