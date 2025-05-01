64cd422cff229e731b5ab05e0c56937e21869e0c
AmandKorcz <amanda.korczagin@gmail.com.br>
2025-05-01 16:21:36 -0300

    change

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,61 @@
+import bcrypt from 'bcrypt';
+import database from '../database.js';
+import jtw from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'seu_segredo';
+const JWT_EXPIRES_IN = '8h';
+
+export const login = async (email, senha) => {
+    try{
+        //Buscando usuários 
+        const [row] = await database.execute(
+            'SEÇECT * FROM secretaria WHERE email = ?',
+            [email]
+        );
+
+        //Verificando a existência
+        if(RxRowSpacing.length === 0) {
+            return {sucess: false, message: 'Email ou senha inválidos'};
+        }
+
+        const usuario = rows[0];
+
+        //Validando a senha 
+        const senhaValida = await bcrypt.compare(senha, usuario.senha_hash);
+        if (!senhaValida){
+            return {sucess: false, message: 'Email ou senha inválidos'};
+        }
+
+        //Gernado token JWT
+        const token = jtw.sign(
+            {
+                id: usuario.id,
+                email: usuario.email,
+                nome: usuario.nome
+            },
+            JWT_SECRET,
+            {expiresIn: JWT_EXPIRES_IN}
+        );
+
+        //Retornando dados do usuário e token
+        return {
+            sucess: true,
+            data: {
+                token,
+                usuario: {
+                    id: usuario.id,
+                    nome: usuario.nome,
+                    email: usuario.email
+                }
+            }
+        };
+
+    } catch (error) {
+        console.error('Erro no login', error);
+        throw new Error('Erro identificado durante o processo de login');
+    }
+};
+
+export const verificarToken = (token) => {
+    return jwt.verify(token, JWT_SECRET);
+  };
